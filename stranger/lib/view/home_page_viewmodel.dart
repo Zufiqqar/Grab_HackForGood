323cf7643ac7ed43c14d737d099e7e6d45baaf2c
Leon <56951761+leongkeng@users.noreply.github.com>
2022-06-02 15:30:36 +0800

    example redux

---
+++ stranger/lib/view/home_page_viewmodel.dart
@@ -0,0 +1,6 @@
+// class HomeScreenViewModel {
+//   final SearchState state;
+//   final void Function(String term) onTextChanged;
+//
+//   HomeScreenViewModel({required this.state, required this.onTextChanged});
+// }
