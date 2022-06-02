e16167926bec64f80ba5ccbcb569758a197109c6
Leon <56951761+leongkeng@users.noreply.github.com>
2022-06-02 14:03:14 +0800

    first push landing page

---
+++ stranger/lib/landing_page.dart
@@ -0,0 +1,175 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:stranger/model/colour.dart';
+import 'package:stranger/model/font.dart';
+
+
+class LandingPage extends StatefulWidget {
+  static String tag = '/LandingPage';
+
+  @override
+  LandingPageState createState() => LandingPageState();
+}
+
+class LandingPageState extends State<LandingPage> {
+  var emailCont = TextEditingController();
+  var passwordCont = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    var deviceWidth = MediaQuery.of(context).size.width;
+    var deviceHeight = MediaQuery.of(context).size.height;
+    return Scaffold(
+      body: Container(
+        height: deviceHeight,
+        child: Stack(
+          alignment: Alignment.center,
+          children: <Widget>[
+            // Positioned(
+            //   top: height - (width + width * 0.05),
+            //   child: CachedNetworkImage(
+            //     placeholder: placeholderWidgetFn() as Widget Function(BuildContext, String)?,
+            //     imageUrl: ic_app_background,
+            //     height: width + width * 0.05,
+            //     width: width,
+            //     fit: BoxFit.fill,
+            //   ),
+            // ),
+            SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(24.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    // Image.asset(
+                    //   ic_app_icon,
+                    //   width: width * 0.22,
+                    // ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          'RideWith',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              fontSize: Font().large1,
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold
+                          ),
+                        ),
+                        Text(
+                          'Stranger',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              fontSize: Font().large1,
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: deviceHeight*0.01,
+                    ),
+                    TextFormField(
+                      keyboardType: TextInputType.emailAddress,
+                      autofocus: false,
+                      controller: emailCont,
+                      textCapitalization: TextCapitalization.words,
+                      style: TextStyle(color: Col().ColorPrimary, fontSize: Font().medium1),
+                      decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Col().Splash,
+                          focusColor: Col().blueBackground,
+                          hintText: "Email",
+                          hintStyle: TextStyle(color: Col().orangeBackground, fontSize: Font().medium1),
+                          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+                          focusedBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0),
+                              borderSide: BorderSide(color: Col().AppTextColorPrimary, width: 0.5)),
+                          enabledBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0),
+                              borderSide: BorderSide(color: Colors.transparent, style: BorderStyle.none, width: 0))),
+                    ),
+                    SizedBox(
+                      height: deviceHeight*0.01,
+                    ),
+                    TextFormField(
+                      keyboardType: TextInputType.text,
+                      autofocus: false,
+                      obscureText: true,
+                      style: TextStyle(color: Col().ColorPrimary, fontSize: Font().medium1),
+                      controller: passwordCont,
+                      textCapitalization: TextCapitalization.words,
+                      decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Col().Splash,
+                          focusColor: Col().blueBackground,
+                          hintText: "Password",
+                          hintStyle: TextStyle(color: Col().orangeBackground, fontSize: Font().medium1),
+                          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+                          focusedBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0),
+                              borderSide: BorderSide(color: Col().AppTextColorPrimary, width: 0.5)),
+                          enabledBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0),
+                              borderSide: BorderSide(color: Colors.transparent, style: BorderStyle.none, width: 0))),
+                    ),
+                    SizedBox(
+                      height: deviceHeight*0.01,
+                    ),
+                    SizedBox(
+                      width: double.infinity,
+                      height: 50,
+                      // height: double.infinity,
+                      child: MaterialButton(
+                        padding: EdgeInsets.all(8.0),
+                        child: Text(
+                          'Sign Up',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              fontSize: Font().large1,
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold
+                          ),
+                        ),
+                        textColor: Col().AppTextColorPrimary,
+                        shape: RoundedRectangleBorder(borderRadius: new BorderRadius.circular(40.0)),
+                        color: Col().AppTextColorPrimary,
+                        onPressed: () => {
+                          // ShHomeScreen().launch(context),
+                        },
+                      ),
+                    ),
+                    SizedBox(height: deviceHeight*0.01),
+                    SizedBox(
+                      width: double.infinity,
+                      height: 50,
+                      // height: double.infinity,
+                      child: MaterialButton(
+                        padding: EdgeInsets.all(8.0),
+                        child: Text(
+                          'Sign Up',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              fontSize: Font().large1,
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold
+                          ),
+                        ),
+                        textColor: Col().newDarkGreen,
+                        shape: RoundedRectangleBorder(borderRadius: new BorderRadius.circular(40.0),
+                            side: BorderSide(color: Col().purpleBackground, width: 1)),
+                        color: Col().AppTextColorPrimary,
+                        onPressed: () => {
+                          // ShSignUp().launch(context)
+                        },
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ stranger/lib/model/font.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Font {
+  final double h0 = 40;
+  final double h1 = 32;
+  final double xlarge = 26;
+  final double large2 = 22;
+  final double large1 = 20;
+  final double medium2 = 18;
+  final double medium1 = 16;
+  final double small = 14;
+  final double mini = 12;
+  final double xmini = 10;
+
+}
